b050fd4dd590bbfb9b43d76e7da5b1e17f9b8b87
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2014-05-14 16:55:06 +0200

    Missing Header. Oops.

---
+++ include/polli/Options.h
@@ -0,0 +1,20 @@
+//===--------------- polli/Options.h - The Polli option category *- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// Introduce an option category for Polli.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef POLLI_OPTIONS_H
+#define POLLI_OPTIONS_H
+
+#include "llvm/Support/CommandLine.h"
+
+extern llvm::cl::OptionCategory PolliCategory;
+#endif
