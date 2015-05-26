d811465c89136b5475ac71f103944af1408b0404
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2015-05-26 17:43:18 +0200

    pprof-analyze: make pprof-analyze process the whole experiment group at once
    The old version was behaving as if it was reading a I/O stream, which
    complicated things. The new pprof-analyze code operates on a whole
    experiment at once and aggregates the data as before.
    
    File I/O & CSV I/O will be deprecated soon.

--- include/pprof/pgsql.h
+++ include/pprof/pgsql.h
@@ -4,15 +4,21 @@
 #include "pprof/pprof.h"
 #include <vector>
 #include <map>
+#include <set>
 
 namespace pprof {
 namespace pgsql {
 
-void StoreRun(Run<PPEvent> &Events, const Options &opts);
-bool ReadRun(Run<PPEvent> &Events,
-             std::map<uint32_t, PPStringRegion> &Regions, const Options &opt);
-
+using IdVector = std::vector<uint32_t>;
+using UuidSet = std::set<std::string>;
 using Metrics = std::map<std::string, double>;
+
+UuidSet ReadAvailableRunGroups();
+IdVector ReadAvailableRunIDs(std::string run_group);
+Run<PPEvent> ReadRun(uint32_t run_id,
+                     std::map<uint32_t, PPStringRegion> &Regions);
+
+void StoreRun(Run<PPEvent> &Events, const pprof::Options &opts);
 void StoreRunMetrics(long run_id, const Metrics &M);
 }
 }
