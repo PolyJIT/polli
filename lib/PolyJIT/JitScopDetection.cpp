1994074aff04b8093a130e96e062f53c29d602e8
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2015-07-10 15:05:21 +0200

    jsd: clang format

--- lib/PolyJIT/JitScopDetection.cpp
+++ lib/PolyJIT/JitScopDetection.cpp
@@ -68,8 +68,10 @@ private:
   unsigned EntryLine, ExitLine;
 
 public:
-  DiagnosticJitScopFound(Function &F, std::string FileName, unsigned EntryLine, unsigned ExitLine) : DiagnosticInfo(PluginDiagnosticKind, DS_Note), F(F),
-  FileName(FileName), EntryLine(EntryLine), ExitLine(ExitLine) {}
+  DiagnosticJitScopFound(Function &F, std::string FileName, unsigned EntryLine,
+                         unsigned ExitLine)
+      : DiagnosticInfo(PluginDiagnosticKind, DS_Note), F(F), FileName(FileName),
+        EntryLine(EntryLine), ExitLine(ExitLine) {}
 
   void print(DiagnosticPrinter &DP) const override;
   static bool classof(const DiagnosticInfo *DI) {
