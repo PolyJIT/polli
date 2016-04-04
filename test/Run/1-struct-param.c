728cb46eb15d440bb70ec30e1a76df8b50e16e3d
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2016-04-04 16:09:31 +0200

    tests: adjust unit tests to new output structure
    * polly-process-unprofitable -> polli-process-unprofitable
    * missing diagnostic error output (The information about the required parameters
    is equivalent to the diagnostic output, so we just remove the test for the
    diagnostics).

--- test/Run/1-struct-param.c
+++ test/Run/1-struct-param.c
@@ -1,4 +1,4 @@
-// RUN: %clang -O2 -Xclang -load -Xclang LLVMPolyJIT.so -mllvm -polly-process-unprofitable -mllvm -polli -mllvm -jitable -mllvm -polly-detect-keep-going %s -mllvm -polli-analyze -o %t -lpjit 2>&1 | FileCheck %s -check-prefix=STATIC
+// RUN: %clang -O2 -Xclang -load -Xclang LLVMPolyJIT.so -mllvm -polli-process-unprofitable -mllvm -polli -mllvm -jitable -mllvm -polly-detect-keep-going %s -mllvm -polli-analyze -o %t -lpjit 2>&1 | FileCheck %s -check-prefix=STATIC
 // RUN: %t 2>&1 | FileCheck %s
 #include <stdio.h>
 
@@ -30,8 +30,6 @@ int main(int argc, char **argv) {
 // STATIC: 1 regions require runtime support:
 // STATIC:   0 region for.body => for.cond.cleanup requires 1 params
 // STATIC:     0 - (4 * (sext i32 %n to i64))
-// STATIC:     1 reasons can be fixed at run time:
-// STATIC:       0 - Non affine access function: {0,+,(4 * (sext i32 %n to i64))}<nsw><%for.body>
 
 // CHECK: 1 1 1 1 1 0 0 0 0 0
 // CHECK: 3 1 3 1 5 0 3 0 7 0
