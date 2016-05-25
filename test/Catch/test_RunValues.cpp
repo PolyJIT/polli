4f0e6505f96daacd1d5c900393f83e1d41e5ce4c
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2016-05-25 12:01:21 +0200

    tests: add more runValues() tests

--- test/Catch/test_RunValues.cpp
+++ test/Catch/test_RunValues.cpp
@@ -46,14 +46,13 @@ using polli::RunValueList;
 using polli::runValues;
 
 SCENARIO("runValues() can align parameter values to run-time values", "[run]") {
+  LLVMContext Ctx;
+  Module M("test_RunValues", Ctx);
+
   GIVEN("A Specializer Request for the function 'void f(int a)' with the"
         "arguments a=1") {
-    LLVMContext Ctx;
-    Module M("test_RunValues", Ctx);
-
-    SmallVector<Type*, 2> Args;
+    SmallVector<Type*, 1> Args;
     Args.push_back(Type::getInt64Ty(Ctx));
-
     Function *F =
         Function::Create(FunctionType::get(Type::getVoidTy(Ctx), Args, false),
                          GlobalValue::ExternalLinkage, "test_run_values", &M);
@@ -78,6 +77,51 @@ SCENARIO("runValues() can align parameter values to run-time values", "[run]") {
       THEN("The first element of the RunValueList has the value 1.") {
         REQUIRE(*reinterpret_cast<int64_t *>(RVs[0].value) == 1);
       }
+
+      THEN("The argument type of the first parameter is Int64.") {
+        Type *Ty = RVs[0].Arg->getType();
+        REQUIRE(Ty->isIntegerTy());
+        REQUIRE(Ty->getIntegerBitWidth() == 64);
+      }
     }
+    std::free(A);
   }
+
+  GIVEN("A SpecializerRequest for the function void f(int64_t, int64_t *)") {
+    SmallVector<Type*, 2> Args;
+    Args.push_back(Type::getInt64Ty(Ctx));
+    Args.push_back(Type::getInt64PtrTy(Ctx));
+    Function *F =
+        Function::Create(FunctionType::get(Type::getVoidTy(Ctx), Args, false),
+                         GlobalValue::ExternalLinkage, "test_run_values_2", &M);
+
+    int64_t *A = reinterpret_cast<int64_t *>(std::malloc(2*sizeof(int64_t*)));
+    int64_t i = 2;
+    int64_t *B = reinterpret_cast<int64_t *>(std::malloc(sizeof(int64_t)));
+
+    A[0] = reinterpret_cast<int64_t>(&i);
+    A[1] = reinterpret_cast<int64_t>(&B);
+
+    SpecializerRequest R(nullptr, 2, reinterpret_cast<char **>(A));
+    R.F = F;
+
+    WHEN("runValues() is called") {
+      RunValueList RVs = runValues(R);
+      THEN("The returned list has size 2") {
+        REQUIRE(RVs.size() == 2);
+      }
+
+      THEN("The first element of the RunValueList has the value 2.") {
+        REQUIRE(*reinterpret_cast<int64_t *>(RVs[0].value) == 2);
+      }
+
+      THEN("The second element of the RunValueList is of type 'Pointer'.") {
+        Type *Ty = RVs[1].Arg->getType();
+        REQUIRE(Ty->isPointerTy());
+      }
+    }
+    std::free(A);
+    std::free(B);
+  }
+
 }
