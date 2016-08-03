8120d56cafeba9cf5c2be5cc3a60244d608306c0
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2016-08-03 13:11:28 +0200

    tasks: remove comment.

--- include/polli/Tasks.h
+++ include/polli/Tasks.h
@@ -108,7 +108,6 @@ public:
 class TaskSystem {
 private:
   const unsigned Count{std::thread::hardware_concurrency() - 1};
-  //const unsigned Count{1};
   std::vector<std::thread> Threads;
   std::vector<polli::JobQueue> JobQs{Count};
   std::atomic<unsigned> Index{0};
