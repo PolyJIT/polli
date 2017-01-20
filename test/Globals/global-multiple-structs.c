ca3fd1d2f88832a5b1d7b558f0a16ce3cf316652
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2017-01-20 14:22:56 +0100

    tests: Fix Globals/ test-cases after global-var handling change

--- test/Globals/global-multiple-structs.c
+++ test/Globals/global-multiple-structs.c
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -emit-llvm -O2 -load LLVMPolyJIT.so -mllvm -polli -mllvm -jitable -mllvm -polli-process-unprofitable -o /dev/null %s -mllvm -polli-analyze -mllvm -stats 2>&1 | FileCheck %s
+// RUN: %clang_cc1 -emit-llvm -O2 -load LLVMPolyJIT.so -mllvm -polli -mllvm -jitable -mllvm -polli-process-unprofitable -o /dev/stderr %s -mllvm -polli-analyze -mllvm -stats 2>&1 | FileCheck %s
 
 // Check that we can handle a single global variable during compilation.
 
@@ -21,23 +21,24 @@ int main(int argc, char **argv) {
 
   return StructB.A[0];
 }
+// CHECK: @StructA = common local_unnamed_addr global %struct.TestA zeroinitializer, align 4
+// CHECK: @StructB = common local_unnamed_addr global %struct.TestA zeroinitializer, align 4
 
 // CHECK: polyjit.entry:
-// CHECK-NEXT:   %params = alloca [4 x i8*]
-// CHECK-NEXT:   %2 = alloca i32
-// CHECK-NEXT:   store volatile i32 %0, i32* %2
-// CHECK-NEXT:   %3 = getelementptr [4 x i8*], [4 x i8*]* %params, i32 0, i32 0
-// CHECK-NEXT:   %4 = bitcast i32* %2 to i8*
-// CHECK-NEXT:   store i8* %4, i8** %3
-// CHECK-NEXT:   %5 = alloca i64
-// CHECK-NEXT:   store volatile i64 %1, i64* %5
-// CHECK-NEXT:   %6 = getelementptr [4 x i8*], [4 x i8*]* %params, i32 0, i32 1
-// CHECK-NEXT:   %7 = bitcast i64* %5 to i8*
-// CHECK-NEXT:   store i8* %7, i8** %6
-// CHECK-NEXT:   %8 = getelementptr [4 x i8*], [4 x i8*]* %params, i32 0, i32 2
-// CHECK-NEXT:   store i8* bitcast (%struct.TestA* @StructA to i8*), i8** %8
-// CHECK-NEXT:   %9 = getelementptr [4 x i8*], [4 x i8*]* %params, i32 0, i32 3
-// CHECK-NEXT:   store i8* bitcast (%struct.TestA* @StructB to i8*), i8** %9
-// CHECK-NEXT:   %10 = bitcast [4 x i8*]* %params to i8*
-// CHECK-NEXT:   %11 = call i1 @pjit_main(i8* getelementptr inbounds ([{{[0-9]+}} x i8], [{{[0-9]+}} x i8]* @test{{[0-9a-zA-Z\.\_]+}}.pjit.scop.prototype, i32 0, i32 0), i32 4, i8* %10)
-// CHECK-NEXT:   br i1 %11, label %polyjit.ready, label %polyjit.not.ready
+// CHECK-NEXT:   %params = alloca [3 x i8*], align 8
+// CHECK-NEXT:   %2 = alloca i32, align 4
+// CHECK-NEXT:   store volatile i32 %n, i32* %2, align 4
+// CHECK-NEXT:   %3 = bitcast [3 x i8*]* %params to i32**
+// CHECK-NEXT:   store i32* %2, i32** %3, align 8
+// CHECK-NEXT:   %4 = alloca i64, align 8
+// CHECK-NEXT:   store volatile i64 %0, i64* %4, align 8
+// CHECK-NEXT:   %5 = getelementptr [3 x i8*], [3 x i8*]* %params, i64 0, i64 1
+// CHECK-NEXT:   %6 = bitcast i8** %5 to i64**
+// CHECK-NEXT:   store i64* %4, i64** %6, align 8
+// CHECK-NEXT:   %7 = alloca i64, align 8
+// CHECK-NEXT:   store volatile i64 %1, i64* %7, align 8
+// CHECK-NEXT:   %8 = getelementptr [3 x i8*], [3 x i8*]* %params, i64 0, i64 2
+// CHECK-NEXT:   %9 = bitcast i8** %8 to i64**
+// CHECK-NEXT:   store i64* %7, i64** %9, align 8
+// CHECK-NEXT:   %10 = bitcast [3 x i8*]* %params to i8*
+// CHECK-NEXT:   %11 = call i1 @pjit_main(i8* getelementptr inbounds ([2157 x i8], [2157 x i8]* @test_0.pjit.scop.prototype, i64 0, i64 0), i64* getelementptr inbounds ({ i64, i64, i64, i1, i64, i64 }, { i64, i64, i64, i1, i64, i64 }* @polyjit.stats.test_0.pjit.scop.1, i64 0, i32 0), i32 3, i8* %10) #3
