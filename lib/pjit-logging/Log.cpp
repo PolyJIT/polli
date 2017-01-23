1fd86725491bfae0faa195c52655f948ed545f5d
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2017-01-23 13:40:01 +0100

    log: redirect output to stderr, if we do not set a filename

--- lib/pjit-logging/Log.cpp
+++ lib/pjit-logging/Log.cpp
@@ -10,7 +10,7 @@ static const char *getLogOutFile() {
   if (const char *LogF = std::getenv("POLLI_LOG_FILE")) {
     return LogF;
   }
-  return "/tmp/.polyjit";
+  return nullptr;
 }
 
 static inline std::vector<spdlog::sink_ptr> &global_init() {
@@ -20,13 +20,14 @@ static inline std::vector<spdlog::sink_ptr> &global_init() {
     return sinks;
   init = true;
 
-  const char *LOG_FILENAME = getLogOutFile();
-  const size_t LOG_SIZE = 1048576 * 100;
-
-  spdlog::set_async_mode(1048576);
-  sinks.push_back(std::make_shared<spdlog::sinks::rotating_file_sink_mt>(
-      LOG_FILENAME, "log", LOG_SIZE, 5, true));
-  // sinks.push_back(std::make_shared<spdlog::sinks::stderr_sink_mt>());
+  if (const char *LOG_FILENAME = getLogOutFile()) {
+    const size_t LOG_SIZE = 1048576 * 100;
+    spdlog::set_async_mode(1048576);
+    sinks.push_back(std::make_shared<spdlog::sinks::rotating_file_sink_mt>(
+        LOG_FILENAME, "log", LOG_SIZE, 5, true));
+  } else {
+    sinks.push_back(std::make_shared<spdlog::sinks::stderr_sink_mt>());
+  }
   return sinks;
 }
 
